a0231d3bcbed719c7a6802f9e7a4f02d33431367
Wern Ancheta <ancheta.wern@gmail.com>
2018-09-19 08:46:10 +0800

    Update Root.js
    Supress timer warnings https://github.com/facebook/react-native/issues/12981#issuecomment-302059217

--- Root.js
+++ Root.js
@@ -5,6 +5,8 @@ import HomeScreen from "./src/screens/HomeScreen";
 import ShareScreen from "./src/screens/ShareScreen";
 import ViewScreen from "./src/screens/ViewScreen";
 
+console.ignoredYellowBox = ["Setting a timer"];
+
 export default createStackNavigator(
   {
     Home: HomeScreen,
